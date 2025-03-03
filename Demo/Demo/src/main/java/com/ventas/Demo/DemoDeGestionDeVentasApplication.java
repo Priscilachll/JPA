b7ed6cb65dba0bf0391eceb651f80904f3ed73bd
Priscilachll <128869937+Priscilachll@users.noreply.github.com>
2025-03-03 19:56:41 -0300

    Add files via upload

---
+++ Demo/Demo/src/main/java/com/ventas/Demo/DemoDeGestionDeVentasApplication.java
@@ -0,0 +1,15 @@
+package com.ventas.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class DemoDeGestionDeVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeGestionDeVentasApplication.class, args);
+	}
+
+}
---
+++ Demo/Demo/src/test/java/com/ventas/Demo/DemoDeGestionDeVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventas.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeGestionDeVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
